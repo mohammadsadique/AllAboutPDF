ee48cb870a286c840de359d11905c6bc76f5fc82
Mohammad Sadique <69007302+mohammadsadique@users.noreply.github.com>
2022-03-29 17:10:32 +0530

    Add files via upload

---
+++ carrier_instruction.blade.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carrier Invite</title>
+</head>
+<style>
+    @page{
+        size: 600pt 960pt;
+    }
+    body{
+        margin: 7px 25px 0px 25px;
+        font-size: 15px;
+    }
+    
+    hr.multiline {
+        border: none;
+        border-top: 3px double #333;
+        color: #333;
+        overflow: visible;
+        text-align: center;
+        height: 5px;
+    }
+    .content2 {
+        line-height: 2.0;
+    }
+</style>
+<body>
+    
+    <main>
+        <section class="main-body">
+            <div class="img">
+                <img src="http://localhost:4200/assets/images/logo.png" alt="logo">
+            </div>
+            <div style="margin-top:55px;">
+                <hr>
+                <b>Carrier Instructions</b>
+                <hr class="multiline">
+            </div>
+            <div>
+                <p>General Load Instructions:</p>
+                <ul>
+                    <li>Double brokering is not acceptable and will result in non-payment</li>
+                    <li>
+                        All drivers must call for dispatch. Do not send your truck to load before this is signed and emailed back to AAA
+                        Logistic Services, LLC.
+                    </li>
+                    <li>We require a check call from all drivers between 8AM to 10AM, or $50 charge will be imposed</li>
+                    <li>
+                        Late arrivals to an appointment will be subject to a charge of up to $500 and also any other associated fees
+                        imposed by our customer will be imposed on the carrier
+                    </li>
+                    <li>
+                        Accessorial charges (lumper, driver assist, etc.) must be approved by AAA Logistic Services, LLC at the time of pick
+                        up or delivery
+                    </li>
+                    <li>Driver must report any overages, shortages, or pallet damage immediately</li>
+                    <li>We must receive signed BOL within 5 days of delivery or a $50 fee will be imposed</li>
+                    <li>
+                        If documents are not received and consignee is contacted directly by AAA Logistic Services, LLC, a $50 fee will be
+                        imposed
+                    </li>
+                </ul>
+                <p>Payment & Billing:</p>
+                <ul>
+                    <li>
+                        You may bring in all paperwork to our office at 9374 Remick Ave, Arleta, CA 91331 during normal business hours
+                    </li>
+                    <li>Please reference "Load #" on all billing invoices</li>
+                    <li>
+                        You may email all paperwork (invoice, signed rate con, BOL, etc.) to: carriers@aaalogisticservices.com
+                    </li>
+                    <li>
+                        All carriers must specify on their invoice if they wish to be paid "Net 30" or "Quick Pay" (Quick pay charge is 3%)
+                    </li>
+                </ul>
+            </div>
+            <div>
+                AAA Logistic Services, LLC shall at all times, be acting in the capacity of an independent contractor to the Carrier, and doe s
+                not hire, or in any way exercise control, over the carriers drivers or other employees or agent or the Carrier. Carrier will
+                perform transport services for AAA Logistic Services, LLC.; Carrier agrees and understands that they are an “independent
+                contractor” with its own ICC operating authority; Carrier understands that all permits and taxes are its sole responsibility;
+                Carrier agrees that it maintains its own current cargo, liability, and worker’s comp insurance; An original certificate of ins.
+                Naming AAA Logistic Services, LLC as a certificate holder, a copy of ICC authority and this contract must be on file before
+                payment can be made. In the event of a cargo claim, carrier will be liable for the full invoice value of the loss. Carrier will bill
+                AAA Logistic Services, LLC directly for all services provided unless otherwise agreed to in writing. Rate is subject to change if
+                there are any service failures and/or missed deliveries. Carriers consent to pick up shipment acknowledges and constitutes
+                carriers acceptance of the terms and conditions outlined herein.
+
+            </div>
+            <div>
+            <br>
+            <table class="content2">
+                <tr>
+                    <td>
+                        __________________________________ <br>
+                        Carrier Name
+                    </td>
+                    <td>
+                        __________________________________ <br>
+                        MC Number
+
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        __________________________________ <br>
+                        Authorized Signature
+                    </td>
+                    <td>
+                        __________________________________ <br>
+                        Date
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        __________________________________ <br>
+                        Printed Name
+                    </td>
+                    <td>
+                        __________________________________ <br>
+                        Title
+                    </td>
+                </tr>
+            </table>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
